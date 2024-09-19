20182891ebc5e4aa4f58b919dbc435827442b3f5
Daon Park <deeopark39@gmail.com>
2024-09-19 13:01:59 +0900

    feat: use multiprocessing for faster crawling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote import webelement
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+import multiprocessing as mp
 import time
 
 
@@ -27,28 +28,61 @@ def get_conference_cfp(
     driver: webdriver,
     conference: dict[str, str],
     name: str,
-) -> None:
+) -> str:
     url   = conference["url"]
     xpath = conference["xpath"]
     fmt   = conference["fmt"]
     zones = conference["zones"]
 
+    """ access URL """
     driver.get(url)
-    cfp = find_element(driver, By.XPATH, xpath).text.split()
-    cfp = ' '.join(cfp)
 
-    print("[{n:>15}]".format(n=name), end=" ")
+    """ retrieve CfP information """
+    cfp = find_element(driver, By.XPATH, xpath).text.split()
+
+    """ format string """
+    result = "[{n:>15}] ".format(n=name)
     try:
-        cfp, zone = cfp, ""
-        for i in range(zones):
-            cfp, zone = cfp.rsplit(" ", 1)
-        print(datetime.strptime(cfp, fmt))
+        cfp = ' '.join(cfp[:len(cfp) - zones])
+        result += str(datetime.strptime(cfp, fmt))
     except:
-        print(cfp)
+        result += cfp
+
+    return result
+
+
+def main(information) -> str:
+    conference = information[0]
+    name       = information[1]
+    # print("Getting {n}...".format(n=name))
+
+    with Display(visible=False, size=(10,10)):
+        """ set up firefox.service """
+        service = webdriver.firefox.service.Service()
+        service.path = "/usr/bin/geckodriver"
+
+        """ set up firefox.options """
+        options = webdriver.firefox.options.Options()
+        options.headless = True
+
+        """ initialize driver """
+        browser = webdriver.Firefox(service=service, options=options)
+
+        """ crawl CfP information """
+        result = get_conference_cfp(browser, conference, name)
+        browser.close()
+
+    return result
 
 
 if __name__ == "__main__":
     conferences = {
+        "ICML": {
+            "url"  : "https://icml.cc/Conferences/2024/Dates",
+            "xpath": "/html/body/main/div[2]/div/div/div[3]/table/tbody/tr[13]/td[3]/span",
+            "fmt"  : "%b %d '%y",
+            "zones": 3,
+        },
         "NIPS": {
             "url"  : "https://neurips.cc/Conferences/2024",
             "xpath": "/html/body/main/div[2]/div/div/div[7]/table[2]/tbody/tr[4]/td[2]/span[1]",
@@ -86,21 +120,13 @@ if __name__ == "__main__":
             "fmt"  : "%A, %B %d, %Y, %H:%M %p",
             "zones": 0,
         },
+        "PPoPP": {
+            "url"  : "https://ppopp25.sigplan.org",
+            "xpath": "/html/body/div/div[3]/div[1]/div[2]/div[2]/table/tbody/tr[3]/td",
+            "fmt"  : "%a %d %b %Y",
+            "zones": 5,
+        },
     }
 
-    with Display(visible=False, size=(1200,1200)):
-        """ set up firefox.service """
-        service = webdriver.firefox.service.Service()
-        service.path = "/usr/bin/geckodriver"
-
-        """ set up firefox.options """
-        options = webdriver.firefox.options.Options()
-        options.headless = True
-
-        """ initialize driver """
-        browser = webdriver.Firefox(service=service, options=options)
-
-        for name in conferences.keys():
-            get_conference_cfp(browser, conferences[name], name)
-
-        browser.close()
+    results = mp.Pool().map(main, zip(conferences.values(), conferences.keys()))
+    print('\n'.join(results))
