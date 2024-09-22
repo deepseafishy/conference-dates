9a2aa5bab720268c155d55f5a1b96fa783f62097
Daon Park <deeopark39@gmail.com>
2024-09-23 00:02:47 +0900

    fix: iteratively search for datetime string

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def get_conference_cfp(
     url   = conference["url"]
     xpath = conference["xpath"]
     fmt   = conference["fmt"]
-    zones = conference["zones"]
 
     """ access URL """
     driver.get(url)
@@ -42,11 +41,14 @@ def get_conference_cfp(
 
     """ format string """
     result = "[{n:>15}] ".format(n=name)
-    try:
-        cfp = ' '.join(cfp[:len(cfp) - zones])
-        result += str(datetime.strptime(cfp, fmt))
-    except:
-        result += cfp
+    fmt_len, cfp_len = len(fmt.split()), len(cfp)
+    time = ' '.join(cfp)
+    for i in range(cfp_len - fmt_len + 1):
+        try:
+            time = datetime.strptime(' '.join(cfp[i:i + fmt_len]), fmt)
+        except:
+            pass
+    result += str(time)
 
     return result
 
@@ -81,50 +83,42 @@ if __name__ == "__main__":
             "url"  : "https://icml.cc/Conferences/2024/Dates",
             "xpath": "/html/body/main/div[2]/div/div/div[3]/table/tbody/tr[13]/td[3]/span",
             "fmt"  : "%b %d '%y",
-            "zones": 3,
         },
         "NIPS": {
             "url"  : "https://neurips.cc/Conferences/2024",
             "xpath": "/html/body/main/div[2]/div/div/div[7]/table[2]/tbody/tr[4]/td[2]/span[1]",
             "fmt"  : "%B %d '%y %I:%M %p",
-            "zones": 1,
         },
         "Spring EuroSys": {
             "url"  : "https://2025.eurosys.org",
             "xpath": "/html/body/section/div/div/div/ul[1]/li[2]/strong",
             "fmt"  : "%A, %B %d, %Y",
-            "zones": 1,
         },
         "Fall EuroSys": {
             "url"  : "https://2025.eurosys.org",
             "xpath": "/html/body/section/div/div/div/ul[2]/li[2]/strong",
             "fmt"  : "%A, %B %d, %Y",
-            "zones": 1,
         },
         "HPDC": {
             "url"  : "https://www.hpdc.org/2024/calls-cfp.html",
             "xpath": "/html/body/div/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/ul/li[1]/del/p/strong",
             "fmt"  : "%B %d, %Y.",
-            "zones": 0,
         },
         "OSDI": {
             "url"  : "https://www.usenix.org/conference/osdi25/call-for-papers",
             "xpath": "/html/body/div[2]/main/section/div[3]/article/div/div/div/div/div[1]/div/div/div/div/div/div" +
                      "/div/div/div/div/div/div/ul[1]/li[2]/strong",
             "fmt"  : "%A, %B %d, %Y, %H:%M %p",
-            "zones": 4,
         },
         "PACT": {
             "url"  : "https://pact2024.github.io",
             "xpath": "/html/body/div/div/div[2]/ul[1]/li[2]",
-            "fmt"  : "%A, %B %d, %Y, %H:%M %p",
-            "zones": 0,
+            "fmt"  : "%B %d, %Y",
         },
         "PPoPP": {
             "url"  : "https://ppopp25.sigplan.org",
             "xpath": "/html/body/div/div[3]/div[1]/div[2]/div[2]/table/tbody/tr[3]/td",
             "fmt"  : "%a %d %b %Y",
-            "zones": 5,
         },
     }
 
