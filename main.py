412e0fe17e70c1c865abb565af34b6fac8789fa3
Daon Park <deeopark39@gmail.com>
2024-09-23 01:18:40 +0900

    feat: apply time zone and AoE

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote import webelement
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+from zoneinfo import ZoneInfo
 import multiprocessing as mp
 import time
 
@@ -26,6 +27,7 @@ def find_element(
 
 def get_conference_cfp(
     driver: webdriver,
+    zones: dict[str, str],
     conference: dict[str, str],
     name: str,
 ) -> str:
@@ -37,7 +39,12 @@ def get_conference_cfp(
     driver.get(url)
 
     """ retrieve CfP information """
-    cfp = find_element(driver, By.XPATH, xpath).text.split()
+    cfp = find_element(driver, By.XPATH, xpath).text.upper().split()
+
+    """ find timezone """
+    zone = ""
+    for substring in cfp:
+        zone = substring if substring in zones.keys() else zone
 
     """ format string """
     result = "[{n:>15}] ".format(n=name)
@@ -48,12 +55,31 @@ def get_conference_cfp(
             time = datetime.strptime(' '.join(cfp[i:i + fmt_len]), fmt)
         except:
             pass
+
+    if zone == "":
+        """ apply AoE if no timezone is specified """
+        time += timedelta(hours=36)
+        time = time.replace(tzinfo=timezone.utc)
+    else:
+        """ apply timezone if specified """
+        time = time.replace(tzinfo=zones[zone])
+    """ apply KST """
+    time = time.astimezone(zones["KST"])
     result += str(time)
 
     return result
 
 
 def main(information) -> str:
+    zones = {
+        "PDT" : ZoneInfo("America/Los_Angeles"),
+        "PST" : ZoneInfo("America/Los_Angeles"),
+        "CET" : ZoneInfo("Europe/Berlin"),
+        "CEST": ZoneInfo("Europe/Berlin"),
+        "EST" : ZoneInfo("America/New_York"), # UTC-5
+        "KST" : ZoneInfo("Asia/Seoul"),
+    }
+
     conference = information[0]
     name       = information[1]
     # print("Getting {n}...".format(n=name))
@@ -71,7 +97,7 @@ def main(information) -> str:
         browser = webdriver.Firefox(service=service, options=options)
 
         """ crawl CfP information """
-        result = get_conference_cfp(browser, conference, name)
+        result = get_conference_cfp(browser, zones, conference, name)
         browser.close()
 
     return result
@@ -108,7 +134,7 @@ if __name__ == "__main__":
             "url"  : "https://www.usenix.org/conference/osdi25/call-for-papers",
             "xpath": "/html/body/div[2]/main/section/div[3]/article/div/div/div/div/div[1]/div/div/div/div/div/div" +
                      "/div/div/div/div/div/div/ul[1]/li[2]/strong",
-            "fmt"  : "%A, %B %d, %Y, %H:%M %p",
+            "fmt"  : "%A, %B %d, %Y, %I:%M %p",
         },
         "PACT": {
             "url"  : "https://pact2024.github.io",
